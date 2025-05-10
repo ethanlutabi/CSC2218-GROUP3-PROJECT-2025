32ec679b8932dcb9f661f24770eed7fe3ff0f6bd
Alupo Tabitha <tabithaalupo5@gmail.com>
2025-05-10 15:19:32 +0300

    2

--- insfrastructure/models/transactionModel.py
+++
@@ -1,6 +0,0 @@
-class Transaction:
-    def _init_(self, transaction_id: int, account_id: int, amount: float, transaction_type: str):
-        self.transaction_id = transaction_id
-        self.account_id = account_id
-        self.amount = amount
-        self.transaction_type = transaction_type
